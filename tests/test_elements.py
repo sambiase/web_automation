9bc1b902d07584b5c292776cea10af37a03c5aec
Sambiase <marsamb@gmail.com>
2023-01-10 13:30:24 -0300

    changed test file

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,3 +1,8 @@
 def test_page_title(browser_init):
     browser_init.get('https://www.mockaroo.com/')
     assert 'Mockaroo - Random Data Generator and API Mocking Tool | JSON / CSV / SQL / Excel' in browser_init.title
+
+def test_browser_type(browser_init):
+    browser_init.get('https://www.mockaroo.com/')
+    assert 'chrome' in browser_init.name.lower()
+
